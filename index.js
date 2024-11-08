7ee5d422353af6ca89631626f83fe5c4e4b07919
ripul <rdogra@yamaha-motor-india.com>
2024-11-08 12:09:46 +0530

    innitial

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let a = 5
 var b = 10
+var c =100
 function toprint(a){
     console.log(a)
     if(a==b){
         console.log(true)
     }
-    else{
+    else if(c==b){
         console.log(false)
+    }
+    else{
+        console.log("Oh no")
     }
 }
